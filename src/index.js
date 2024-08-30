6a032dd07893dee7c8d67d98fcdf8b2fa2f04a1a
Ro Rakhit <ro@datalign.com>
2024-08-30 13:32:00 -0400

    build(rrakhit-carbon-enablement): need to use packages from carbon repo to build w webpack

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 /**
- * Copyright IBM Corp. 2020
+ * Copyright IBM Corp. 2020, 2024
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
 import './styles.scss';
+import './styles/main.css';
 import { Dropdown } from 'carbon-components';
+import '@carbon/layout/scss/breakpoint';
 
 Dropdown.create(document.getElementById('my-dropdown'));
+alert('connected!');
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+module.exports = {
+  mode: 'none',
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.s[ac]ss$/i, /\.css$/],
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
